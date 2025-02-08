d2f06e099e229aec819a669dbb2f5f3ff77912b3
caaat1 <kgolov@hotmail.com>
2025-02-08 11:28:19 +0300

    Casual

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,24 @@
-// eslint.config.js
+// Relative path: "eslint.config.js"
 module.exports = [
   {
-    files: ['**/*.ts', '**/*.cts', '**/*.mts'],
+    extends: [
+      'plugin:prettier/recommended', // Integrate Prettier with ESLint
+    ],
+    files: ['**/*.{js,cjs,mjs,ts,cts,mts}'],
+    ignores: ['node_modules/**', 'dist/**'],
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: 'module',
       parser: require('@typescript-eslint/parser'),
       parserOptions: {
         project: './tsconfig.json',
+        tsconfigRootDir: __dirname, // Handles relative tsconfig paths
       },
     },
     plugins: {
       '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      import: require('eslint-plugin-import'),
+      prettier: require('eslint-plugin-prettier'),
     },
     rules: {
       ...require('@typescript-eslint/eslint-plugin').configs.recommended.rules,
@@ -24,7 +31,24 @@ module.exports = [
           accessibility: 'explicit',
         },
       ],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+          ],
+          alphabetize: {order: 'asc', caseInsensitive: true},
+        },
+      ],
+      'prettier/prettier': 'error',
+      'padding-line-between-statements': [
+        'error',
+        {blankLine: 'always', prev: 'import', next: '*'},
+        {blankLine: 'any', prev: 'import', next: 'import'},
+      ],
     },
-    ignores: ['node_modules/**', 'dist/**'],
   },
 ];
