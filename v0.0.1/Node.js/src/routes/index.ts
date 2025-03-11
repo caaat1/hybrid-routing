e76600c333df3d0387e96298614077f4bfa0907e
caaat1 <kgolov@hotmail.com>
2025-03-11 16:54:36 +0300

    Before starting to implement protoDOM in TS

--- v0.0.1/Node.js/src/routes/index.ts
+++ v0.0.1/Node.js/src/routes/index.ts
@@ -1,14 +1,37 @@
 // src/routes/index.ts
 
+import {exec} from 'child_process'
+import path from 'path'
+import {fileURLToPath} from 'url'
+
 import {Router} from 'express'
 import type {Request, Response} from 'express'
 
 // import View from '../view/index.js'
 
 function renderHome(_req: Request, res: Response): void {
-  res.render('.', {title: 'Home'})
-  //   const view = View.create()
-  //   res.send(view.serialize())
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+
+  const scriptPath = path.resolve(__dirname, '../../../PHP/index.php')
+  console.log(scriptPath)
+  // exec(`php ${scriptPath}`, (error, stdout, stderr) => {
+  //   if (error) {
+  //     console.error(`Error: ${error.message}`)
+  //     return
+  //   }
+  //   if (stderr) {
+  //     console.error(`Stderr: ${stderr}`)
+  //     return
+  //   }
+  //   const result: unknown = JSON.parse(stdout)
+  //   console.log(result)
+  //   res.send(result)
+  // })
+
+  // res.render('.', {title: 'Home'})
+
+  // const view = View.create()
 }
 
 function renderAbout(_req: Request, res: Response): void {
